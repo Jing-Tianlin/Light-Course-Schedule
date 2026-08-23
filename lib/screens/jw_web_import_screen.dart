3690fd6fbe5d42d1fd0b13cd3e6e2cb65935d528
Jing Tianlin <3150437891@qq.com>
2026-08-23 23:17:40 +0800

    Improve WebView rendering for desktop-only school portal

--- lib/screens/jw_web_import_screen.dart
+++ lib/screens/jw_web_import_screen.dart
@@ -36,6 +36,13 @@ class _JwWebImportScreenState extends State<JwWebImportScreen> {
           },
         ),
       )
+      ..setUserAgent(
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+        'AppleWebKit/537.36 (KHTML, like Gecko) '
+        'Chrome/120.0.0.0 Safari/537.36',
+      )
+      ..enableZoom(true)
+      ..setBackgroundColor(Colors.white)
       ..loadRequest(Uri.parse(JwConfig.casLoginUrl));
   }
 
